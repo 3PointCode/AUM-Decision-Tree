1c039db12b3209ddd0b72e2523e986efc7e679b6
3PointCode <tomasz.piwoda@gmail.com>
2026-04-23 22:46:42 +0200

    Find best threshold test usage + deps

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
-import numpy as np
-import pandas as pd
-from decision_tree import split_dataset, weighted_gini, gini
+from decision_tree import find_best_split
 from data_utils import load_data
 
 def main():
     X, y, df = load_data("data/WineQT.csv")
-    feature_index = 10
-    threshold = 10.0
-    X_left, y_left, X_right, y_right = split_dataset(X, y, feature_index, threshold)
 
-    print("Gini przed podziałem:", gini(y))
-    print("Gini po podziale:", weighted_gini(y_left, y_right))
-    print("Lewa część:", len(y_left))
-    print("Prawa część:", len(y_right))
+    best_feature, best_threshold, best_gini = find_best_split(X, y)
 
-    print("Probe number:", X.shape[0])
-    print("Features number:", X.shape[1])
-    print("Class distribution:", np.bincount(y))
-    print(df.head())
+    print("Best feature:", best_feature)
+    print("Best threshold:", best_threshold)
+    print("Best gini:", best_gini)
 
 if __name__ == "__main__":
     main()
